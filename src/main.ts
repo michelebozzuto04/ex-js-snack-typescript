8ff5c4dc6d606198c5880ac622f53a60fcf6d10d
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-12-15 12:08:43 +0100

    Snack 2 completed

--- src/main.ts
+++ src/main.ts
@@ -46,3 +46,13 @@ Il type alias Dipendente, ha anche i seguenti dati:
 
 emailAziendale → Email assegnata al dipendente (non si può modificare)
 contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”. */
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: "m" | "f",
+  anniDiServizio: number[],
+  readonly emailAziendale: string,
+  contratto: 'indeterminato' | 'determinato' | 'freelance'
+}
